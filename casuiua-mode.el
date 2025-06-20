a51f0ae96710dfebb4bf49372bca9edafd3724ed
amatgil <amatgilvinyes@gmail.com>
2025-06-21 00:52:42 +0200

    Minor cleanup, send to repl

--- casuiua-mode.el
+++ casuiua-mode.el
@@ -1,18 +1,21 @@
 ;;; casuiua-mode.el --- major mode for uiua  -*- lexical-binding: t; -*-
 
-; TODO: Create mode
-; TODO: Create map
-;    - DONE Open repl
-;    - Send data to repl
-; TODO: hook up lsp to mode
-; TODO: syntax highlighting using said lsp
-; TODO: Add command that opens repl and uiua watch on the side (75/25 on height) .. somehow
+; * DONE: Create mode
+; * DONE: Create map
+; ** DONE Open repl
+; ** DONE data to repl
+; * TODO: hook up lsp to mode
+; ** TODO Syntax highlighting 
+; ** TODO inlay hints and reporting
+; * TODO: Add command that opens repl and uiua watch on the side (75/25 on height) .. somehow
 
-(defcustom casuiua-uiua-cli-cmd "uiua")
+(defcustom casuiua-uiua-cli-cmd "uiua"
+  "Command that starts up uiua"
+  :type 'string)
 
 (defun casuiua-open-repl ()
   (interactive)
-  (let* ((buffer (get-buffer-create "*uiua-repl*"))
+  (let* ((buffer (get-buffer))
          (proc-alive (comint-check-proc buffer))
          (process (get-buffer-process buffer)))
     (unless proc-alive ;; recreate if dead
@@ -23,6 +26,7 @@
     (when buffer
       (pop-to-buffer buffer))))
 
+
 (defun casuiua-open-watch ()
   (interactive)
   (let* ((buffer (get-buffer-create "*uiua-watch*"))
@@ -30,16 +34,31 @@
          (process (get-buffer-process buffer)))
     (unless proc-alive ;; recreate if dead
       (with-current-buffer buffer
-      (message "Starting up watch window")
+        (message "Starting up watch window")
         (apply 'make-comint-in-buffer "Uiua Watch" buffer
                casuiua-uiua-cli-cmd nil '("watch"))))
     (when buffer
       (pop-to-buffer buffer))))
 
+(defun casuiua-send-region-to-repl (region-start region-end)
+  (interactive "r")
+  (if (use-region-p)
+      (let* ((buffer (get-buffer-create "*uiua-repl*"))
+             (proc-alive (comint-check-proc buffer))
+             (process (get-buffer-process buffer)))
+        (if buffer
+            (progn
+              (comint-send-region process region-start region-end)
+              (comint-send-string process "\n"))
+          (message "No repl running")))
+    (message "No region active")))
+
+
 (defvar-keymap casuiua-mode-map
   :parent prog-mode-map
-  :doc "Keymap for casuiua"
   "C-c C-r" #'casuiua-open-repl
   "C-c C-w" #'casuiua-open-watch)
 
 (define-derived-mode casuiua-mode prog-mode "CasUiua" "Major mode for Uiua")
+
+(provide 'casuiua-mode)

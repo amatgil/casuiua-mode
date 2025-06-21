ae73ea98f30b54e0c351f288f3f8e18878036a35
amatgil <amatgilvinyes@gmail.com>
2025-06-21 11:12:04 +0200

    Add lsp and tentative eglot conf

--- casuiua-mode.el
+++ casuiua-mode.el
@@ -9,6 +9,7 @@
 ; ** TODO inlay hints and reporting
 ; * TODO: Add command that opens repl and uiua watch on the side (75/25 on height) .. somehow
 
+;;; START casuiua-mode CONFIG
 (defcustom casuiua-uiua-cli-cmd "uiua"
   "Command that starts up uiua"
   :type 'string)
@@ -60,5 +61,50 @@
   "C-c C-w" #'casuiua-open-watch)
 
 (define-derived-mode casuiua-mode prog-mode "CasUiua" "Major mode for Uiua")
+
+;;; END casuiua-mode CONFIG
+
+;;; START LSP CONFIGURATION
+;; (See https://emacs-lsp.github.io/lsp-mode/page/adding-new-language/)
+
+;; "casuiua-mode" corresponds to language "uiua"
+(add-to-list 'lsp-language-id-configuration '(casuiua-mode . "uiua"))
+
+(lsp-register-client (make-lsp-client
+                      :new-connection (lsp-stdio-connection `(,casuiua-uiua-cli-cmd "lsp"))
+                      :activation-fn (lsp-activate-on "uiua")
+                      :server-id 'uiua-lsp))
+
+;; (See https://gist.github.com/magistau/4c12ab54663f911e6a7560a807b63f1c)
+;; (use-package eglot
+;;   :config
+;;   (add-to-list 'eglot-server-programs
+;;                '(uiua-ts-mode . ("uiua" "lsp")))
+;;   (setq-default eglot-workspace-configuration
+;;                 '(:uiua.inlayHints.values :json-false
+;;                   :uiua.inlayHints.bindingSignatureHints t
+;;                   :uiua.inlayHints.inlineSignatureHints t
+;;                   :uiua.inlayHints.inlineHintMinLength 3))
+;;   :hook ((prog-mode . eglot-ensure)))
+;; (use-package eglot-semtok
+;;   :config
+;;   (setq eglot-semtok-faces
+;; 	(append
+;; 	 '("uiua_number" (("" "" (:foreground "#ea5")))
+;; 	   "noadic_function" (("" "" (:foreground "#ed5e6a")))
+;; 	   "monadic_function" (("" "" (:foreground "#95d16a")))
+;; 	   "dyadic_function" (("" "" (:foreground "#54b0fc")))
+;; 	   "triadic_function" (("" "" (:foreground "#8078f1")))
+;; 	   "tetradic_function" (("" "" (:foreground "#f576d8")))
+;; 	   "monadic_modifier" (("" "" (:foreground "#f0c36f")))
+;; 	   "dyadic_modifier" (("" "" (:foreground "#cc6be9")))
+;; 	   "triadic_modifier" (("" "" (:foreground "#F5A9B8")))
+;; 	   "uiua_module" (("" "" (:foreground "#d7be8c"))))
+;; 	 eglot-semtok-faces))
+;;   :hook ((eglot-connect . eglot-semtok-on-connected)
+;; 	 (uiua-ts-mode . eglot-semtok-font-lock-init)))
+
+
+;;; END LSP CONFIGURATION
 
 (provide 'casuiua-mode)
